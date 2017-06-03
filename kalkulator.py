ff62fd677349fa7862e538add988ca336e99960f
Kuba <gromadzki.jakub@wp.pl>
2017-06-03 17:41:11 +0200

    nowa funkcja dodawania gotowa do review

--- kalkulator.py
+++ kalkulator.py
@@ -2,6 +2,10 @@ print('Hello, world')
 def dodawanie(a,b):
 	wynik=a+b
 	return wynik
-	
-dodawanie()
+
+print('WprowadŸ pierwsz¹ liczbê: ")
+z1=int(input))
+print('WprowadŸ drug¹ liczbê: ")
+z2=int(input))
+print(dodawanie(z1,z2))
 
